c92d16b9ba265428086d3d5491362c9ac245a403
Anoop Verma <anoopvrma@gmail.com>
2023-12-18 05:19:56 +0530

    Fix contrast ratio in contact bar header.

--- apps/gurukul/src/components/header/ContactBar.tsx
+++ apps/gurukul/src/components/header/ContactBar.tsx
@@ -15,15 +15,15 @@ function ContactBar() {
             <Container maxWidth="lg">
                 <Toolbar disableGutters>
                     <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }} >
-                        <Typography textAlign="center" variant="caption" sx={{ mr: 2, color: theme.palette.grey[500] }}>Have any questions?</Typography>
-                        <Typography textAlign="center" variant="caption" sx={{ mx: 2, color: theme.palette.grey[500] }}>(+91) 999XXXXXXX</Typography>
-                        <Typography textAlign="center" variant="caption" sx={{ mx: 2, color: theme.palette.grey[500] }}>template@studyminutes.com</Typography>
+                        <Typography textAlign="center" variant="caption" sx={{ mr: 2, color: theme.palette.grey[300] }}>Have any questions?</Typography>
+                        <Typography textAlign="center" variant="caption" sx={{ mx: 2, color: theme.palette.grey[300] }}>(+91) 999XXXXXXX</Typography>
+                        <Typography textAlign="center" variant="caption" sx={{ mx: 2, color: theme.palette.grey[300] }}>template@studyminutes.com</Typography>
                     </Box>
                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }} ></Box>
                     <Box sx={{
                         flexDirection: 'row-reverse'
                     }}>
-                        <Typography textAlign="center" variant="caption" sx={{ mx: 2, color: theme.palette.grey[500] }}>Register/Login</Typography>
+                        <Typography textAlign="center" variant="caption" sx={{ mx: 2, color: theme.palette.grey[300] }}>Register/Login</Typography>
                     </Box>
                 </Toolbar>
             </Container>
